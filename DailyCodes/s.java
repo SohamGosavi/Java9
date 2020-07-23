658751ffbaf9dcd1aa2f246e27beeb0ec9083420
“SohamGosavi” <srgosavi29@gmail.com>
2020-07-23 14:55:05 +0530

    Codes

---
+++ DailyCodes/s.java
@@ -0,0 +1,26 @@
+class Arithmatic{
+
+            int x = 20;
+            int y = 50;
+            Boolean ans ;
+            
+            void Operations(){
+            
+                  System.out.println( "ans =" + (x<y));
+                  System.out.println( "ans =" + (x>y));
+                  System.out.println( "ans =" + (x<=y));
+                  System.out.println( "ans =" + (x>=y));
+                  System.out.println( "ans =" + (x==y));
+                  System.out.println( "ans =" + (x!=y));
+         
+            }   
+
+             public static void main(String[] args ){
+             
+               Operators  obj = new  Operators();
+               obj.Operations();
+             
+             
+             
+             }
+
